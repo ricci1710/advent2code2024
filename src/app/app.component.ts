2028f968ce3da5c7c5acfcbf88ca337fb9e61e4e
RK <ricci1710@gmail.com>
2024-10-24 19:59:39 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Day01Component} from "./day01/day01.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Day01Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/day01/day01.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-day01',
+  standalone: true,
+  imports: [],
+  templateUrl: './day01.component.html',
+  styleUrl: './day01.component.scss'
+})
+export class Day01Component {
+  resultPartOne: number = -1;
+  resultPartTwo: number = -1;
+
+  constructor() {
+  }
+}
