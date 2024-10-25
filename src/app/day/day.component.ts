0724a4b7409d8b00b0e96755c75cd43b12252cd6
ltirkunz <rico.kunz@usu.com>
2024-10-25 09:23:07 +0200

    preparetion

--- src/app/day/day.component.ts
+++ src/app/day/day.component.ts
@@ -2,19 +2,19 @@ import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
 @Component({
-  selector: 'app-day01',
+  selector: 'app-day',
   standalone: true,
   imports: [],
-  templateUrl: './day01.component.html',
-  styleUrl: './day01.component.scss'
+  templateUrl: './day.component.html',
+  styleUrl: './day.component.scss'
 })
-export class Day01Component implements OnInit {
+export class DayComponent implements OnInit {
   resultPartOne: number = -1;
   resultPartTwo: number = -1;
 
   storeData: string[] = [];
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private dayNumber: number) {
   }
 
   ngOnInit(): void {
