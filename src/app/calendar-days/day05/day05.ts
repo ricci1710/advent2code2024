b8273a7f82e0688d89ca93060cf2d86a9759688e
Rico Kunz <ricci1710@gmail.com>
2024-12-06 07:12:02 +0100

    resolved day 1

--- src/app/calendar-days/day05/day05.ts
+++ src/app/calendar-days/day05/day05.ts
@@ -1,5 +1,10 @@
 import {DayBase} from "../../utlils/DayBase";
 
+type PageData = {
+  ruleMapper: Map<number, number[]>;
+  pageMapper: number[][];
+};
+
 export class Day05 extends DayBase {
   /**
    *
@@ -7,7 +12,9 @@ export class Day05 extends DayBase {
    */
   calcPartOne(): number {
     const storeData: string[] = this.getStoreData();
-    return -1;
+    const pageData: PageData = this.scan(storeData);
+
+    return this.calc(pageData);
   }
 
   /**
@@ -16,5 +23,58 @@ export class Day05 extends DayBase {
   calcPartTwo(): number {
     const storeData: string[] = this.getStoreData();
     return -1;
+  }
+
+  private scan(storeData: string[]): PageData {
+    const ruleMapper = new Map<number, number[]>();
+    const pageMapper: number[][] = [];
+
+    let rules = true;
+    storeData.forEach((item) => {
+      if (item.trim() === '') {
+        rules = false;
+      } else if (rules) {
+        const infos = item.split('|');
+        const pageNumber: number = parseInt(infos[0].trim(), 10);
+        const behindPage: number = parseInt(infos[1].trim(), 10);
+
+        const pages = ruleMapper.get(pageNumber) ?? [];
+        pages.push(behindPage);
+        ruleMapper.set(pageNumber, pages);
+      } else {
+        const infos: number[] = item.split(',').map(x => parseInt(x.trim(), 10));
+        pageMapper.push(infos);
+      }
+    });
+
+    return {ruleMapper, pageMapper};
+  }
+
+  private calc(pageData: PageData): number {
+    const {ruleMapper, pageMapper} = pageData;
+
+    const result: number[][] = [];
+
+    pageMapper.forEach((item) => {
+      let correctRuleset = true;
+      for (let i = 0; i < item.length; i++) {
+        const pageNumber: number = item[i];
+        const rule = ruleMapper.get(pageNumber);
+        if (rule) {
+          for (let j = i + 1; j < item.length; j++) {
+            const behindPage: number = item[j];
+            if (!rule.includes(behindPage)) {
+              correctRuleset = false;
+            }
+          }
+        }
+      }
+      if (correctRuleset) {
+        result.push(item);
+      }
+    });
+
+    console.log(result);
+    return -10
   }
 }
